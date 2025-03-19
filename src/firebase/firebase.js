f147f19a07da43f31334471e3a69304e04e4fe5f
PlainMyth <stomeyer4@gmail.com>
2025-03-19 12:11:59 -0700

    Added an environment to prevent API leak.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,23 +1,20 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getAuth} from "firebase/auth";
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAmdJ2YBbx50c9DvH6ojnI1nHPuNDcUf0w",
-  authDomain: "hakari-spin.firebaseapp.com",
-  projectId: "hakari-spin",
-  storageBucket: "hakari-spin.firebasestorage.app",
-  messagingSenderId: "639762671199",
-  appId: "1:639762671199:web:91222bdf84131b3a21ddea",
-  measurementId: "G-E6YMP50CZN"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const auth = getAuth(app)
+const auth = getAuth(app);
 
 export { app, auth };
